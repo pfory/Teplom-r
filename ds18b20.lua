bb6f48a506140074c6f0653041c428996c8d8028
pfory <pfory@seznam.cz>
2016-05-04 07:59:58 +0200

    +novejsi verze knihovny

--- ds18b20.lua
+++ ds18b20.lua
@@ -3,8 +3,8 @@
 -- NODEMCU TEAM
 -- LICENCE: http://opensource.org/licenses/MIT
 -- Vowstar <vowstar@nodemcu.com>
+-- 2015/02/14 sza2 <sza2trash@gmail.com> Fix for negative values
 --------------------------------------------------------------------------------
-
 -- Set module name as parameter of require
 local modname = ...
 local M = {}
@@ -36,98 +36,101 @@ C = 0
 F = 1
 K = 2
 function setup(dq)
-  pin = dq
-  if(pin == nil) then
-    pin = defaultPin
-  end
-  ow.setup(pin)
+pin = dq
+if(pin == nil) then
+pin = defaultPin
 end
-
+ow.setup(pin)
+end
 function addrs()
-  setup(pin)
-  tbl = {}
-  ow.reset_search(pin)
-  repeat
-    addr = ow.search(pin)
-    if(addr ~= nil) then
-      table.insert(tbl, addr)
-    end
-    tmr.wdclr()
-  until (addr == nil)
-  ow.reset_search(pin)
-  return tbl
+setup(pin)
+tbl = {}
+ow.reset_search(pin)
+repeat
+addr = ow.search(pin)
+if(addr ~= nil) then
+table.insert(tbl, addr)
 end
-
+tmr.wdclr()
+until (addr == nil)
+ow.reset_search(pin)
+return tbl
+end
 function readNumber(addr, unit)
-  result = nil
-  setup(pin)
-  flag = false
-  if(addr == nil) then
-    ow.reset_search(pin)
-    count = 0
-    repeat
-      count = count + 1
-      addr = ow.search(pin)
-      tmr.wdclr()
-    until((addr ~= nil) or (count > 100))
-    ow.reset_search(pin)
-  end
-  if(addr == nil) then
-    return result
-  end
-  crc = ow.crc8(string.sub(addr,1,7))
-  if (crc == addr:byte(8)) then
-    if ((addr:byte(1) == 0x10) or (addr:byte(1) == 0x28)) then
-      -- print("Device is a DS18S20 family device.")
-      ow.reset(pin)
-      ow.select(pin, addr)
-      ow.write(pin, 0x44, 1)
-      -- tmr.delay(1000000)
-      present = ow.reset(pin)
-      ow.select(pin, addr)
-      ow.write(pin,0xBE,1)
-      -- print("P="..present)
-      data = nil
-      data = string.char(ow.read(pin))
-      for i = 1, 8 do
-        data = data .. string.char(ow.read(pin))
-      end
-      -- print(data:byte(1,9))
-      crc = ow.crc8(string.sub(data,1,8))
-      -- print("CRC="..crc)
-      if (crc == data:byte(9)) then
-        if(unit == nil or unit == C) then
-          t = (data:byte(1) + data:byte(2) * 256) * 625
-        elseif(unit == F) then
-          t = (data:byte(1) + data:byte(2) * 256) * 1125 + 320000
-        elseif(unit == K) then
-          t = (data:byte(1) + data:byte(2) * 256) * 625 + 2731500
-        else
-          return nil
-        end
-        t = t / 10000
-        -- print("Temperature="..t1.."."..t2.." Centigrade")
-        -- result = t1.."."..t2
-        return t
-      end
-      tmr.wdclr()
-    else
-    -- print("Device family is not recognized.")
-    end
-  else
-  -- print("CRC is not valid!")
-  end
-  return result
+result = nil
+setup(pin)
+flag = false
+if(addr == nil) then
+ow.reset_search(pin)
+count = 0
+repeat
+count = count + 1
+addr = ow.search(pin)
+tmr.wdclr()
+until((addr ~= nil) or (count > 100))
+ow.reset_search(pin)
 end
-
+if(addr == nil) then
+return result
+end
+crc = ow.crc8(string.sub(addr,1,7))
+if (crc == addr:byte(8)) then
+if ((addr:byte(1) == 0x10) or (addr:byte(1) == 0x28)) then
+-- print("Device is a DS18S20 family device.")
+ow.reset(pin)
+ow.select(pin, addr)
+ow.write(pin, 0x44, 1)
+-- tmr.delay(1000000)
+present = ow.reset(pin)
+ow.select(pin, addr)
+ow.write(pin,0xBE,1)
+-- print("P="..present)
+data = nil
+data = string.char(ow.read(pin))
+for i = 1, 8 do
+data = data .. string.char(ow.read(pin))
+end
+-- print(data:byte(1,9))
+crc = ow.crc8(string.sub(data,1,8))
+-- print("CRC="..crc)
+if (crc == data:byte(9)) then
+t = (data:byte(1) + data:byte(2) * 256)
+if (t > 32767) then
+t = t - 65536
+end
+if (addr:byte(1) == 0x28) then
+t = t * 625 -- DS18B20, 4 fractional bits
+else
+t = t * 5000 -- DS18S20, 1 fractional bit
+end
+if(unit == nil or unit == 'C') then
+-- do nothing
+elseif(unit == 'F') then
+t = t * 1.8 + 320000
+elseif(unit == 'K') then
+t = t + 2731500
+else
+return nil
+end
+t = t / 10000
+return t
+end
+tmr.wdclr()
+else
+-- print("Device family is not recognized.")
+end
+else
+-- print("CRC is not valid!")
+end
+return result
+end
 function read(addr, unit)
-  t = readNumber(addr, unit)
-  if (t == nil) then
-    return nil
-  else
-    return t
-  end
+t = readNumber(addr, unit)
+if (t == nil) then
+return nil
+else
+return t
 end
-
+end
 -- Return module table
 return M
